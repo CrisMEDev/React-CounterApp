190b7baf1aea5c88e1ced548e3707fb85ae2a740
Cristian Morales <cr15moresp@gmail.com>
2021-09-02 17:04:48 -0500

    Functional component CounterApp creado

---
+++ src/CounterApp.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CounterApp = ({ value }) => {
+
+    return (
+        <>
+            <h1>Counter App</h1>
+            <h2>{ value }</h2>
+        </>
+    );
+
+}
+
+CounterApp.propTypes = {
+    value: PropTypes.number
+}
+
+CounterApp.defaultProps = {
+    value: 0
+}
+
+
+
+export default CounterApp;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import PrimeraApp2 from './PrimeraApp';
+// import PrimeraApp2 from './PrimeraApp';
+import CounterApp from './CounterApp';
 
 // Referencias en el html
 const divRoot = document.querySelector('#root');
 
-ReactDOM.render( <PrimeraApp2 saludo='Hola, soy Cristian ME' numero={123} />, divRoot );
+// ReactDOM.render( <PrimeraApp2 saludo='Hola, soy Cristian ME' numero={123} />, divRoot );
+ReactDOM.render( <CounterApp />, divRoot );
 
 
 
