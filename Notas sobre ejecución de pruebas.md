aac4eeaffd3641c107a3ca67133f7c833c71235b
Cristian Morales <cr15moresp@gmail.com>
2021-09-11 12:38:36 -0500

    Usando expect-toBe para verificar una prueba

--- Notas sobre ejecución de pruebas.md
+++ Notas sobre ejecución de pruebas.md
@@ -1,3 +1,5 @@
 # Test
 
 Para correr los test se usa ```npm run test```
+
+Cada uno de los archivos de pureba se conoce como 'test suite'
--- src/tests/demo.test.js
+++ src/tests/demo.test.js
@@ -1,11 +1,20 @@
 
-// El nombre del test debe describir que es lo que estamos probando
-test( 'debe de ser true', () => {
+describe('Pruebas en el archivo demo.test.js', () => {
 
-    const isActive = false;
+    // El nombre del test debe describir que es lo que estamos probando
+    test( 'deben ser iguales los strings', () => {
+    
+        // 1. Inicialización
+        const mensaje = 'Hola mundo';
+    
+        // 2. Estímulo
+        const mensaje2 = `Hola mundo`;
+    
+        // 3. Observar el comportamiento
+        expect( mensaje ).toBe( mensaje2 ); // Hace la comparación ===
+    
+    })
 
-    if (!isActive){
-        throw new Error('No está activo');
-    }
+});
 
-})
+
