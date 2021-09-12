f9762c6ba62c35d18b9ac606c0cbb7f803413cec
Cristian Morales <cr15moresp@gmail.com>
2021-09-12 00:06:24 -0500

    Se usó enzyme para realizar unit test al componente PrimeraApp

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,18 @@
 
+import Enzyme from 'enzyme';
+
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+import {createSerializer} from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+
+
+
+
+
+
 // Para lograr que expect trabaje con el DOM de React
-import '@testing-library/jest-dom/extend-expect';
+// import '@testing-library/jest-dom/extend-expect';
 
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,22 +1,35 @@
-import {render} from '@testing-library/react'
+// import {render} from '@testing-library/react'
+
+import { shallow } from 'enzyme';
+
 import PrimeraApp2 from '../PrimeraApp';
 
 describe('Pruebas en <PrimeraApp />', () => {
 
-    test('Debe de mostrar el mensaje "Hola, Soy Cristian ME"', () => {
+    // test('Debe de mostrar el mensaje "Hola, Soy Cristian ME"', () => {
         
+    //     const saludo = 'Hola, Soy Cristian ME';
+
+    //     const wrapper = render( <PrimeraApp2 saludo={saludo} numero={0} /> );
+
+    //     // Concatener !!! hace que la prueba falle pero es dificil de leer el error por eso se usará enzyme
+    //     // expect(wrapper.getByText( saludo + '!!!' )).toBeInTheDocument();
+
+    //     expect(wrapper.getByText( saludo )).toBeInTheDocument();
+
+    // });
+    
+    test('Debe de mostrar <PrimeraApp /> correctamente', () => {
+
+        const numero = 0;
         const saludo = 'Hola, Soy Cristian ME';
+        const wrapper = shallow( <PrimeraApp2 saludo={saludo} numero={numero} /> );
 
-        const wrapper = render( <PrimeraApp2 saludo={saludo} numero={0} /> );
-
-        // Concatener !!! hace que la prueba falle pero es dificil de leer el error por eso se usará enzyme
-        // expect(wrapper.getByText( saludo + '!!!' )).toBeInTheDocument();
-
-        expect(wrapper.getByText( saludo )).toBeInTheDocument();
-
-    });
+        expect( wrapper ).toMatchSnapshot();
+        
+    })
     
-    
+
 });
 
 
